59bd7bc0896a5967961a42cf266b3e018a0844d0
daisuke0207 <daisuke02074869@gmail.com>
2020-09-27 10:27:10 +0900

    ユーザー管理の実装

---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :user do
+    nickname { Faker::Name.name }
+    email { Faker::Internet.unique.free_email }
+    # password {Faker::Internet.password(min_length: 6)}
+    password { 'abc456' }
+    password_confirmation { password }
+    first_name { 'あ' }
+    last_name { '色' }
+    first_name_kana { 'ア' }
+    last_name_kana  { 'イロ' }
+    birthday { Faker::Date.birthday }
+  end
+end
