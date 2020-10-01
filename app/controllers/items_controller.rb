bdfff0c4ec0d4e8c044f1991cc5dcbdd3c6a43b6
daisuke0207 <daisuke02074869@gmail.com>
2020-10-01 15:21:15 +0900

    コードの整理

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :move_to_session, only: [:new, :edit]
-
+  before_action :set_item, only: [:show, :edit, :update]
   def index
     @items = Item.includes(:user).order('created_at DESC')
   end
@@ -19,16 +19,13 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     redirect_to root_path unless current_user.id == @item.user_id
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(params_item)
       redirect_to item_path(params[:id])
     else
@@ -44,5 +41,9 @@ class ItemsController < ApplicationController
 
   def move_to_session
     redirect_to new_user_session_path unless user_signed_in?
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
