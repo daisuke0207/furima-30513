81f8b6572461888d044cbd97aa6c103d78efb4c8
daisuke0207 <daisuke02074869@gmail.com>
2020-10-01 14:50:35 +0900

    商品情報編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :move_to_session, only: :new
+  before_action :move_to_session, only: [:new, :edit]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -20,6 +20,20 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+    redirect_to root_path unless current_user.id == @item.user_id
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(params_item)
+      redirect_to item_path(params[:id])
+    else
+      render :edit
+    end
   end
 
   private
